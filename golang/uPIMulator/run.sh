97266e05b42fb50da08fa987850ab33a86ec19c5
Patrick Li <ilovegithub@patrickli.mozmail.com>
2025-05-13 17:04:39 -0400

    feat: BFS testcase added (still VA actually)

---
+++ golang/uPIMulator/run.sh
@@ -0,0 +1,2 @@
+./build/uPIMulator --root_dirpath ~/Space/uPIMulator/golang/uPIMulator --bin_dirpath ~/Space/uPIMulator/golang/uPIMulator/bin --benchmark BFS --num_channels 1 --num_ranks_per_channel 1 --num_dpus_per_rank 1 --num_tasklets 16 --data_prep_params 524288
+
