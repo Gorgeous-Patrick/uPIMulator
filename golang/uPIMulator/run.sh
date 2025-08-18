70ef7363382e5391744ad8a9aebdd8fcec206149
Baichuan Li <patrli@umich.edu>
2025-08-17 22:37:11 -0400

    fix

--- golang/uPIMulator/run.sh
+++ golang/uPIMulator/run.sh
@@ -3,5 +3,6 @@ mkdir bin
 pushd script || exit 1
 python3 build.py || exit 1
 popd || exit 1
-./build/uPIMulator --root_dirpath /home/patrickli/Space/uPIMulator/golang/uPIMulator --bin_dirpath /home/patrickli/Space/uPIMulator/golang/uPIMulator/bin --benchmark BFS --num_channels 1 --num_ranks_per_channel 1 --num_dpus_per_rank 1 --num_tasklets 16 --data_prep_params 1024
+echo hhh$PWD
+./build/uPIMulator --root_dirpath $PWD --bin_dirpath $PWD/bin --benchmark BFS --num_channels 1 --num_ranks_per_channel 1 --num_dpus_per_rank 1 --num_tasklets 16 --data_prep_params 1024
 
